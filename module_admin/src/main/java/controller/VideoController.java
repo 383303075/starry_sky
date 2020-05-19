78afd1d318173361b8f6589b2693dbbaa7e0de80
383303075 <bwq383303075@163.com>
2020-05-19 15:59:21 +0800

    2020.5.19_15:58

---
+++ module_admin/src/main/java/controller/VideoController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import service.IBaseService;
+import service.IUserService;
+import service.IVideoService;
+
+@Controller
+@RequestMapping("video")
+public class VideoController extends BaseController{
+    @Autowired
+    IVideoService videoService;
+
+    @RequestMapping("addPage")
+    public String addPage(){
+        return "X-admin/member-add";
+    }
+
+
+//    @RequestMapping("/getAdminInfo")
+//    public String getAdminInfo(){
+//
+//        return "";
+//    }
+
+    @Override
+    public IBaseService getService() {
+        return videoService;
+    }
+}
---
+++ module_admin/src/main/java/service/impl/VideoServiceImpl.java
@@ -0,0 +1,19 @@
+package service.impl;
+
+import dao.IBaseDao;
+import dao.IVideoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import service.IVideoService;
+
+@Service
+public class VideoServiceImpl extends BaseServiceImpl implements IVideoService {
+
+    @Autowired
+    IVideoDao videoDao;
+
+    @Override
+    public IBaseDao getDao() {
+        return videoDao;
+    }
+}
