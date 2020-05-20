045e7277f2c23fcd35ea4ac832b52c8ad7e673b5
743508772 <743508772@qq.com>
2020-05-20 08:52:56 +0800

    dajilao

--- module_admin/src/main/java/controller/VideoController.java
+++ module_admin/src/main/java/controller/VideoController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import service.IBaseService;
-import service.IUserService;
 import service.IVideoService;
 
 @Controller
