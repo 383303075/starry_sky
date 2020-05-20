71c0ebd568791c9c1b3042da743c791575a2ce12
zzzzzzzxj <3258793014@qq.com>
2020-05-20 10:05:34 +0800

    hello world

--- module_admin/src/main/java/util/GetUUID.java
+++ module_admin/src/main/java/util/GetUUID.java
@@ -3,6 +3,10 @@ package util;
 import java.util.UUID;
 
 public class GetUUID {
+    /**
+     * djfldshffkfjkdj
+     * @return
+     */
     public static String getUUID(){
         return UUID.randomUUID().toString().replace("-","");
     }
