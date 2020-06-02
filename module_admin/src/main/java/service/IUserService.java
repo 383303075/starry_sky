ccda7eb40870828b831e8a5a8b538594d7fc1737
383303075 <bwq383303075@163.com>
2020-06-02 09:56:16 +0800

    bwq_无修改2020.6.2

--- module_admin/src/main/java/service/IUserService.java
+++ module_admin/src/main/java/service/IUserService.java
@@ -2,6 +2,9 @@ package service;
 
 import domain.User;
 
+import java.util.Map;
+
 public interface IUserService extends IBaseService{
     User findById(String u_id);
+    int editUser(Map map);
 }
